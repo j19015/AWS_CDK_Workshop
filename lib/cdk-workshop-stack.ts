a2c60c9e04bbbb521565c27ba08bacd47e840e59
j19015 <j19015@sangi.jp>
2023-10-18 01:37:45 +0000

    [add] ec2 package

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,5 +1,7 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+// ec2 に関するパッケージを import
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
