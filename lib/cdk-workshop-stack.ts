1335784223fa093f45ef21967ea033a00af9703d
j19015 <j19015@sangi.jp>
2023-10-18 01:54:57 +0000

    [add] ec2 initicalize

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,7 +1,11 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+
 // ec2 に関するパッケージを import
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+
+// ファイルを読み込むためのパッケージを import
+import { readFileSync } from "fs";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -10,6 +14,35 @@ export class CdkWorkshopStack extends Stack {
     // vpc を宣言
     const vpc = new ec2.Vpc(this, "BlogVpc", {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+    });
+    
+    // EC2 インスタンスの宣言を準備
+    const webServer1 = new ec2.Instance(this, "WordpressServer1", {
+      // EC2 インスタンスを起動する VPC を設定
+      vpc,
+      
+      // t2.small インスタンスタイプを指定
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
+      
+      // AmazonLinuxImage インスタンスを生成し、AMI を設定
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      // EC2 インスタンスを配置するサブネットを指定
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+    });
+    
+    // user-data.sh を読み込み、変数に格納
+    const script = readFileSync("./lib/resources/user-data.sh", "utf8");
+    // EC2 インスタンスにユーザーデータを追加
+    webServer1.addUserData(script);
+    
+    // port80, 全ての IP アドレスからのアクセスを許可
+    webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+    
+    // EC2 インスタンスアクセス用の IP アドレスを出力
+    new CfnOutput(this, "WordpressServer1PublicIPAddress", {
+      value: `http://${webServer1.instancePublicIp}`,
     });
     
   }
